e404c5c740fd16e8420f189864b3b373c8b53bdc
fuadmuradov <1fuad.muradov@gmail.com>
2022-03-03 16:55:34 +0400

    Course and Course Details

---
+++ EduHome/Controllers/CourseController.cs
@@ -0,0 +1,36 @@
+﻿using EduHome.Models;
+using EduHome.Models.DbTables;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly MyContext db;
+
+        public CourseController(MyContext context)
+        {
+            db = context;
+        }
+
+
+        public async Task<IActionResult>  Courses()
+        {
+           List<Models.DbTables.Course> crs = await db.Courses.ToListAsync();
+            return View(crs);
+        }
+
+        public async Task<IActionResult> CourseDetails(int? id)
+        {
+            if (id == null) id = db.Courses.First().id;
+            Models.DbTables.Course course = await db.Courses.Include(x=>x.Category).Include(x=>x.CourseFeatures).Include(x=>x.Compaigns).FirstOrDefaultAsync(x => x.id == id);
+
+            return View(course);
+        }
+    }
+}
