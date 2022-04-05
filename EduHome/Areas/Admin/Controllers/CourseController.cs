fd6cd07d3c53ad466a30da931d48e57c31f25a89
fuadmuradov <1fuad.muradov@gmail.com>
2022-04-05 17:29:18 +0400

    CourseController

---
+++ EduHome/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,60 @@
+﻿using EduHome.Models;
+using EduHome.Models.DbTables;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class CourseController : Controller
+    {
+        private readonly MyContext context;
+        private readonly IWebHostEnvironment webHost;
+
+        public CourseController(MyContext context, IWebHostEnvironment webHost)
+        {
+            this.context = context;
+            this.webHost = webHost;
+        }
+
+        public IActionResult Category()
+        {
+            List<Category> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Category(string Name)
+        {
+            Category category = new Category();
+            category.Name = Name;
+            context.Categories.Add(category);
+             context.SaveChanges();
+
+            return View(context.Categories.ToList());
+        }
+
+        public async Task<IActionResult> DeleteCategory(int Id)
+        {
+            Category existcategory = await context.Categories.FirstOrDefaultAsync(x => x.id == Id);
+            if (existcategory == null) return NotFound();
+            context.Categories.Remove(existcategory);
+            await context.SaveChangesAsync();
+
+            return LocalRedirect("/Admin/Course/Category");
+        }
+
+
+        public IActionResult CoursePage()
+        {
+            return View();
+        }
+    }
+}
--- EduHome/Models/DbTables/Course.cs
+++ EduHome/Models/DbTables/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,23 @@ namespace EduHome.Models.DbTables
     public class Course
     {
         public int id { get; set; }
+        [Required]
+        [StringLength(maximumLength:200)]
         public string Name { get; set; }
         public string Image { get; set; }
+        [Required]
+        [StringLength(maximumLength: 5000)]
         public string Description { get; set; }
+        [Required]
+        [StringLength(maximumLength: 5000)]
         public string About { get; set; }
+        [Required]
+        [StringLength(maximumLength: 5000)]
         public string Apply { get; set; }
+        [StringLength(maximumLength: 1000)]
         public string Certification { get; set; }
         
+        [Required]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
 
